3ddc6f4fb36cd2bc89e4bb044136b05bf5c06db1
dibest <cyrilmmesoma8@gmail.com>
2025-10-27 16:30:10 +0100

    second commit

--- transfer.php
+++ transfer.php
@@ -1,5 +1,54 @@
 <?php
     session_start();
+
+    if(!isset($_SESSION['user'])){
+        header('Location: ./login.php');
+        exit;
+    }
+
+    $user = $_SESSION['user'];
+    $message = '';
+
+    if($_POST){
+        $to_account_number = $_POST ['account_number'];
+        $amount = $_POST['amount'];
+
+        $get_database_file = file_get_contents('./database.json');
+        $database = json_decode($get_database_file, true);
+
+        $is_the_user_found = false;
+        foreach($database['users'] as &$reciever){
+            if($reciever['account_number'] == $to_account_number){
+                $is_the_user_found = true;
+                $debited = false;
+                if($user['balance'] >= $amount){
+                    foreach($database['users'] as &$sender){
+                        if($sender['email'] == $user['email']){
+                            $sender['balance'] = $sender['balance'] - $amount;
+                            $debited = true;
+                        }
+                    }
+                }else{
+                    $message = 'Insuffient Funds';
+                }
+                if($debited == true){
+                    $reciever['balance'] = $reciever['balance'] - $amount;
+                    $message = 'Transfer successful';
+                }
+            }
+        }
+        if($is_the_user_found == false){
+            $message = 'No user with that account number';
+        }
+        $encode_database = json_encode($database);
+        file_put_contents('./database.json', $encode_database);
+
+        foreach($database['users'] as $updated_user){
+            if($updated_user['email'] == $user['email']){
+                $_SESSION['user'] = $updated_user;
+            }
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,10 +80,13 @@
 
     <main>
         <h1>Transfer To Account</h1>
-        <form>
+        <form method="post">
+            <label style="text-align: center;"><?php echo $message ?></label>
             <label>Account Number</label>
-            <input type="text"/>
-            <button>Transfer</button>
+            <input type="text" name= "account_number"/>
+            <label>Amount</label>
+            <input type="text" name="amount"/>
+            <button>Send</button>
         </form>
     </main>
     
